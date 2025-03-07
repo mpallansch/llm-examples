83596b13beb61d749c3accd80c8c89229cc6bbfb
mpallansch <matthew.pallansch@gmail.com>
2025-03-06 20:29:55 -0500

    Changes API Endpoint to Azure OpenAI

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,8 @@
-from openai import OpenAI
+from openai import AzureOpenAI
 import streamlit as st
 
 with st.sidebar:
+    openai_api_endpoint = st.text_input("OpenAI API Endpoint", key="chatbot_api_endpoint", type="password")
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
@@ -16,14 +17,18 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+    if not openai_api_key or not openai_api_endpoint:
+        st.info("Please add your OpenAI API endpoint and key to continue.")
         st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
+    client = AzureOpenAI(
+        api_key = openai_api_key,
+        api_version = "2024-02-01",
+        azure_endpoint = openai_api_endpoint
+    )
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
