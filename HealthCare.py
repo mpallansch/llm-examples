4da36a6cbe0de7a4b85a17846fd1d6c9315afb76
mpallansch <matthew.pallansch@gmail.com>
2025-03-06 23:15:29 -0500

    Changes Logic to Rewrite Article Contents By Type

---
+++ HealthCare.py
@@ -0,0 +1,31 @@
+from openai import AzureOpenAI
+import streamlit as st
+
+with st.sidebar:
+    st.text_input("OpenAI API Endpoint", key="chatbot_api_endpoint", type="password")
+    st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+st.title("Health Care Provider")
+st.caption("Takes the content of an article written for a technical audience and rewrites it for health care providers.")
+
+prompt = st.text_area(
+    "Article contents...",
+)
+
+if st.button("Rewrite"):
+    if not st.session_state["chatbot_api_key"] or not  st.session_state["chatbot_api_endpoint"]:
+            st.info("Please add your OpenAI API endpoint and key to continue.")
+            st.stop()
+
+    client = AzureOpenAI(
+        api_key = st.session_state["chatbot_api_key"],
+        api_version = "2024-02-01",
+        azure_endpoint = st.session_state["chatbot_api_endpoint"]
+    )
+
+    response = client.chat.completions.create(model="gpt-4o", messages=[
+        {"role": "system", "content": "The following is an article written for a technical audience. Rewrite it for health care providers. Use plain language."},
+        {"role": "user", "content": prompt}
+    ])
+    msg = response.choices[0].message.content
+    st.write(msg)
